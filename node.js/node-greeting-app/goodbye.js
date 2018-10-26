967dc19fc455e2be7a45182d6ef5d2181c291927
Ryan <ryanmwb@gmail.com>
2018-10-26 18:36:46 -0400

    finish basic-node-app checkpoint and exercise

---
+++ node.js/node-greeting-app/goodbye.js
@@ -0,0 +1,6 @@
+exports.goodbyeEnglish = function(){
+    console.log("Goodbye")
+};
+exports.goodbyeSpansih = function(){
+    console.log("Adios")
+};
---
+++ node.js/node-greeting-app/main.js
@@ -0,0 +1,10 @@
+const greetings = require('./greeting.js');
+const goodbyes = require('./goodbye')
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+
+goodbyes.goodbyeEnglish();
+goodbyes.goodbyeSpansih();
